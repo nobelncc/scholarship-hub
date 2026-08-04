dd385426537e4b10802497aa6949149413660951
nobelncc <nobelncc@gmail.com>
2026-08-04 19:04:09 +0600

    Add scholarship sources to source_registry.py

---
+++ source_registry.py
@@ -0,0 +1,107 @@
+SOURCES = [
+
+    {
+        "name": "DAAD",
+        "country": "Germany",
+        "type": "official",
+        "url": "https://www.daad.de/en/studying-in-germany/scholarships/"
+    },
+
+    {
+        "name": "Erasmus Mundus",
+        "country": "Europe",
+        "type": "official",
+        "url": "https://erasmus-plus.ec.europa.eu/opportunities/individuals/students/erasmus-mundus-joint-masters"
+    },
+
+    {
+        "name": "Chevening",
+        "country": "United Kingdom",
+        "type": "official",
+        "url": "https://www.chevening.org/scholarships/"
+    },
+
+    {
+        "name": "Commonwealth Scholarships",
+        "country": "United Kingdom",
+        "type": "official",
+        "url": "https://cscuk.fcdo.gov.uk/scholarships/"
+    },
+
+    {
+        "name": "Fulbright",
+        "country": "United States",
+        "type": "official",
+        "url": "https://foreign.fulbrightonline.org/"
+    },
+
+    {
+        "name": "Australia Awards",
+        "country": "Australia",
+        "type": "official",
+        "url": "https://www.dfat.gov.au/people-to-people/australia-awards"
+    },
+
+    {
+        "name": "MEXT",
+        "country": "Japan",
+        "type": "official",
+        "url": "https://www.studyinjapan.go.jp/en/smap-stopj-applications/scholarships/"
+    },
+
+    {
+        "name": "Global Korea Scholarship",
+        "country": "South Korea",
+        "type": "official",
+        "url": "https://www.studyinkorea.go.kr/"
+    },
+
+    {
+        "name": "Türkiye Scholarships",
+        "country": "Türkiye",
+        "type": "official",
+        "url": "https://www.turkiyeburslari.gov.tr/"
+    },
+
+    {
+        "name": "Stipendium Hungaricum",
+        "country": "Hungary",
+        "type": "official",
+        "url": "https://stipendiumhungaricum.hu/"
+    },
+
+    {
+        "name": "Swedish Institute",
+        "country": "Sweden",
+        "type": "official",
+        "url": "https://si.se/en/apply/scholarships/"
+    },
+
+    {
+        "name": "Swiss Government Excellence Scholarships",
+        "country": "Switzerland",
+        "type": "official",
+        "url": "https://www.sbfi.admin.ch/en/swiss-government-excellence-scholarships"
+    },
+
+    {
+        "name": "Government of Ireland",
+        "country": "Ireland",
+        "type": "official",
+        "url": "https://hea.ie/funding-governance-performance/funding-programmes/government-of-ireland-international-education-scholarships/"
+    },
+
+    {
+        "name": "Eiffel Excellence Scholarship",
+        "country": "France",
+        "type": "official",
+        "url": "https://www.campusfrance.org/en/eiffel-scholarship-program-of-excellence"
+    },
+
+    {
+        "name": "China Scholarship Council",
+        "country": "China",
+        "type": "official",
+        "url": "https://www.csc.edu.cn/"
+    }
+]
