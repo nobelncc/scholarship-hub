afd2cfa23436cb14111ec756d85bbbb8009696d5
nobelncc <nobelncc@gmail.com>
2026-08-04 19:07:02 +0600

    Refactor scholarship data structure in source_registry.py

--- source_registry.py
+++ source_registry.py
@@ -1,107 +1,194 @@
 SOURCES = [
 
+    # =========================
+    # EUROPE
+    # =========================
+
     {
         "name": "DAAD",
-        "country": "Germany",
-        "type": "official",
-        "url": "https://www.daad.de/en/studying-in-germany/scholarships/"
+        "provider": "German Academic Exchange Service",
+        "destination": ["Germany"],
+        "source_type": "official",
+        "url": "https://www.daad.org.uk/en/find-funding/scholarship-database/",
     },
 
     {
         "name": "Erasmus Mundus",
-        "country": "Europe",
-        "type": "official",
-        "url": "https://erasmus-plus.ec.europa.eu/opportunities/individuals/students/erasmus-mundus-joint-masters"
+        "provider": "European Union",
+        "destination": ["Europe"],
+        "source_type": "official",
+        "url": "https://erasmus-plus.ec.europa.eu/opportunities/individuals/students/erasmus-mundus-joint-masters",
     },
 
     {
         "name": "Chevening",
-        "country": "United Kingdom",
-        "type": "official",
-        "url": "https://www.chevening.org/scholarships/"
+        "provider": "UK Foreign, Commonwealth & Development Office",
+        "destination": ["United Kingdom"],
+        "source_type": "official",
+        "url": "https://www.chevening.org/scholarships/",
     },
 
     {
         "name": "Commonwealth Scholarships",
-        "country": "United Kingdom",
-        "type": "official",
-        "url": "https://cscuk.fcdo.gov.uk/scholarships/"
+        "provider": "Commonwealth Scholarship Commission",
+        "destination": ["United Kingdom"],
+        "source_type": "official",
+        "url": "https://cscuk.fcdo.gov.uk/scholarships/",
     },
 
     {
-        "name": "Fulbright",
-        "country": "United States",
-        "type": "official",
-        "url": "https://foreign.fulbrightonline.org/"
+        "name": "Gates Cambridge",
+        "provider": "University of Cambridge",
+        "destination": ["United Kingdom"],
+        "source_type": "official",
+        "url": "https://www.gatescambridge.org/scholarships/",
     },
 
     {
-        "name": "Australia Awards",
-        "country": "Australia",
-        "type": "official",
-        "url": "https://www.dfat.gov.au/people-to-people/australia-awards"
-    },
-
-    {
-        "name": "MEXT",
-        "country": "Japan",
-        "type": "official",
-        "url": "https://www.studyinjapan.go.jp/en/smap-stopj-applications/scholarships/"
-    },
-
-    {
-        "name": "Global Korea Scholarship",
-        "country": "South Korea",
-        "type": "official",
-        "url": "https://www.studyinkorea.go.kr/"
-    },
-
-    {
-        "name": "Türkiye Scholarships",
-        "country": "Türkiye",
-        "type": "official",
-        "url": "https://www.turkiyeburslari.gov.tr/"
-    },
-
-    {
-        "name": "Stipendium Hungaricum",
-        "country": "Hungary",
-        "type": "official",
-        "url": "https://stipendiumhungaricum.hu/"
-    },
-
-    {
-        "name": "Swedish Institute",
-        "country": "Sweden",
-        "type": "official",
-        "url": "https://si.se/en/apply/scholarships/"
+        "name": "Rhodes Scholarships",
+        "provider": "Rhodes Trust",
+        "destination": ["United Kingdom"],
+        "source_type": "official",
+        "url": "https://www.rhodeshouse.ox.ac.uk/scholarships/",
     },
 
     {
         "name": "Swiss Government Excellence Scholarships",
-        "country": "Switzerland",
-        "type": "official",
-        "url": "https://www.sbfi.admin.ch/en/swiss-government-excellence-scholarships"
+        "provider": "Swiss Government",
+        "destination": ["Switzerland"],
+        "source_type": "official",
+        "url": "https://www.sbfi.admin.ch/en/swiss-government-excellence-scholarships",
     },
 
     {
-        "name": "Government of Ireland",
-        "country": "Ireland",
-        "type": "official",
-        "url": "https://hea.ie/funding-governance-performance/funding-programmes/government-of-ireland-international-education-scholarships/"
+        "name": "Swedish Institute",
+        "provider": "Swedish Institute",
+        "destination": ["Sweden"],
+        "source_type": "official",
+        "url": "https://si.se/en/apply/scholarships/",
     },
 
     {
         "name": "Eiffel Excellence Scholarship",
-        "country": "France",
-        "type": "official",
-        "url": "https://www.campusfrance.org/en/eiffel-scholarship-program-of-excellence"
+        "provider": "Campus France",
+        "destination": ["France"],
+        "source_type": "official",
+        "url": "https://www.campusfrance.org/en/france-excellence-eiffel-scholarship-program",
     },
 
     {
-        "name": "China Scholarship Council",
-        "country": "China",
-        "type": "official",
-        "url": "https://www.csc.edu.cn/"
-    }
+        "name": "Government of Ireland International Education Scholarships",
+        "provider": "Higher Education Authority Ireland",
+        "destination": ["Ireland"],
+        "source_type": "official",
+        "url": "https://hea.ie/policy/internationalisation/goi-ies/",
+    },
+
+    {
+        "name": "Stipendium Hungaricum",
+        "provider": "Hungarian Government",
+        "destination": ["Hungary"],
+        "source_type": "official",
+        "url": "https://stipendiumhungaricum.hu/",
+    },
+
+
+    # =========================
+    # NORTH AMERICA
+    # =========================
+
+    {
+        "name": "Fulbright Foreign Student Program",
+        "provider": "Fulbright",
+        "destination": ["United States"],
+        "source_type": "official",
+        "url": "https://foreign.fulbrightonline.org/",
+    },
+
+    {
+        "name": "Vanier Canada Graduate Scholarships",
+        "provider": "Government of Canada",
+        "destination": ["Canada"],
+        "source_type": "official",
+        "url": "https://vanier.gc.ca/",
+    },
+
+
+    # =========================
+    # ASIA
+    # =========================
+
+    {
+        "name": "MEXT",
+        "provider": "Government of Japan",
+        "destination": ["Japan"],
+        "source_type": "official",
+        "url": "https://www.studyinjapan.go.jp/en/planning/scholarships/mext-scholarships/",
+    },
+
+    {
+        "name": "Global Korea Scholarship",
+        "provider": "Government of South Korea",
+        "destination": ["South Korea"],
+        "source_type": "official",
+        "url": "https://www.studyinkorea.go.kr/eng/plan/scholarship.do",
+    },
+
+    {
+        "name": "Türkiye Scholarships",
+        "provider": "Government of Türkiye",
+        "destination": ["Türkiye"],
+        "source_type": "official",
+        "url": "https://www.turkiyeburslari.gov.tr/",
+    },
+
+    {
+        "name": "Chinese Government Scholarship",
+        "provider": "China Scholarship Council",
+        "destination": ["China"],
+        "source_type": "official",
+        "url": "https://www.csc.edu.cn/",
+    },
+
+
+    # =========================
+    # AUSTRALIA / NEW ZEALAND
+    # =========================
+
+    {
+        "name": "Australia Awards",
+        "provider": "Australian Government",
+        "destination": ["Australia"],
+        "source_type": "official",
+        "url": "https://www.dfat.gov.au/people-to-people/australia-awards/australia-awards-scholarships",
+    },
+
+    {
+        "name": "New Zealand Scholarships",
+        "provider": "New Zealand Government",
+        "destination": ["New Zealand"],
+        "source_type": "official",
+        "url": "https://www.nzscholarships.govt.nz/",
+    },
+
+
+    # =========================
+    # OTHER
+    # =========================
+
+    {
+        "name": "ADB-Japan Scholarship Program",
+        "provider": "Asian Development Bank",
+        "destination": ["Asia", "Pacific"],
+        "source_type": "official",
+        "url": "https://www.adb.org/work-with-us/careers/japan-scholarship-program",
+    },
+
+    {
+        "name": "World Bank Scholarships",
+        "provider": "World Bank",
+        "destination": ["Global"],
+        "source_type": "official",
+        "url": "https://www.worldbank.org/en/programs/scholarships",
+    },
 ]
